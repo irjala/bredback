009b6e1607254a7a33f96d4ae9133b7a96ab68d0
irjala <jonasirjala@gmail.com>
2021-04-03 17:13:29 +0300

    etc

--- projekt1/upload.php
+++ projekt1/upload.php
@@ -1,4 +1,4 @@
-<form action="index.php" method="post" enctype="multipart/form-data">
+<form action="" method="POST" enctype="multipart/form-data">
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload Image" name="submitupload">
 </form>
